8e6eddadd771570aac9165a85ac17839b6a55254
Mikhail-Pershin <126860715+Mikhail-Pershin@users.noreply.github.com>
2023-09-22 11:55:25 +0300

    1

--- CalculatorComplexNumbers/src/FileLogger.java
+++
@@ -1,5 +0,0 @@
-public class FileLogger implements CalculationObserver {
-    public void onCalculation(ComplexNumber num1, ComplexNumber num2, ComplexOperation operation, ComplexNumber result) {
-        // Логика записи в файл
-    }
-}
